9221a2ee67ce65fd699afbd5f045b3ad3786bc13
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-16 15:04:46 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "11615722"))
     API_HASH = os.environ.get("API_HASH", "c992746520e8886d3330de2ec9a1a3a7")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5655590355:AAGJu4JlThJ4WXOxpclgnJKH351xgZJiYYc")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "@screenshot2_bot")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001898159090"))
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://scre:scre@cluster0.yzmsx7i.mongodb.net/?retryWrites=true&w=majority")
     AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "5596825598").split(" ")]
