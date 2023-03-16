cff5206b6e5f0b278f7df473faf445204e919727
MLZ-BOTZ <125456401+MLZ-BOTZ@users.noreply.github.com>
2023-03-16 14:55:45 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,13 +3,13 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID", "11615722"))
+    API_HASH = os.environ.get("API_HASH", "c992746520e8886d3330de2ec9a1a3a7")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5655590355:AAGJu4JlThJ4WXOxpclgnJKH351xgZJiYYc")
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001898159090"))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://scre:scre@cluster0.yzmsx7i.mongodb.net/?retryWrites=true&w=majority")
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "5596825598").split(" ")]
     MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
